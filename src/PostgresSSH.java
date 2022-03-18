cd0e5a596a29b0505690c05e0f64a4fecb23a423
Chen Lin <cl5613@rit.edu>
2022-03-18 10:39:02 -0400

    commit and push PostgresSSH

--- src/PostgresSSH.java
+++ src/PostgresSSH.java
@@ -12,9 +12,9 @@ public class PostgresSSH {
         int lport = 5432;
         String rhost = "starbug.cs.rit.edu";
         int rport = 5432;
-        String user = "cl5613"; //change to your username
-        String password = "sk#YnG@49i7wYxc"; //change to your password
-        String databaseName = "p320_26"; //change to your database name
+        String user = "YOUR_CS_USERNAME"; //change to your username
+        String password = "YOUR_CS_PASSWORD"; //change to your password
+        String databaseName = "YOUR_DB_NAME"; //change to your database name
 
         String driverName = "org.postgresql.Driver";
         Connection conn = null;
