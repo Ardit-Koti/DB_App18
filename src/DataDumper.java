7a0aaa7115d9b10f8ec605e07423eb69e97870a2
Ardit <ak1935@rit.edu>
2022-03-19 20:22:21 -0400

    A lot of datadump setup progress

--- src/DataDumper.java
+++ src/DataDumper.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.Object.*;
 import java.nio.Buffer;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.Scanner;
 
 /*
@@ -16,7 +18,32 @@ public class DataDumper
     static private String[] Genres;
     static private String[] Studios;
     static private String[] Actors;
-    static void MovieTransfer()
+    static private String[] Directors;
+    static private String[] ReleaseDate;
+    static private String MovieName;
+    static private String MovieID;
+    static private String MPAA;
+    static private String duration;
+    static private int a;
+    static private final String quotes = "\"";
+
+    static private String[] Group(int i, String[] stuff)
+    {
+        int beg = a;
+        while(a < stuff.length)
+        {
+            if(stuff[a].startsWith(quotes))
+            {
+                break;
+            }
+            a++;
+        }
+        String[] list = new String[a-beg+1];
+        System.arraycopy(stuff, beg, list, a, list.length);
+        return list;
+    }
+
+    public static void MovieTransfer(Connection conn)
     {
         BufferedReader reader = null;
         try {
@@ -26,9 +53,60 @@ public class DataDumper
             {
                 String line = reader.readLine();
                 String[] tokens = line.split(",");
+                a = 0;
+                MovieID = tokens[a];
+                a++;
+                if(tokens[a].startsWith(quotes))
+                {
+                    Studios = Group(a, tokens);
+                }
+                else
+                {
+                    Studios = new String[1];
+                    Studios[0] = tokens[a];
+                }
+                a++;
+                if(tokens[a].startsWith(quotes))
+                {
+                    Genres = Group(a, tokens);
+                }
+                else
+                {
+                    Genres = new String[1];
+                    Genres[0] = tokens[a];
+                }
+                a++;
+                MovieName = tokens[a];
+                a++;
+                MPAA = tokens[a];
+                a++;
+                if(tokens[a].startsWith(quotes))
+                {
+                    ReleaseDate = Group(a, tokens);
+                }
+                a++;
+                duration = tokens[a];
+                a++;
+                if(tokens[a].startsWith(quotes))
+                {
+                    Directors = Group(a, tokens);
+                }
+                else
+                {
+                    Directors = new String[1];
+                    Directors[0] = tokens[a];
+                }
+                a++;
+                if(tokens[a].startsWith(quotes))
+                {
+                    Actors = Group(a, tokens);
+                }
+                else
+                {
+                    Actors = new String[1];
+                    Actors[0] = tokens[a];
+                }
             }
         }catch (Exception ignored){};
-
-
     }
 }
