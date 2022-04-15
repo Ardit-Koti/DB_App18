475eed518d437035c9978a61debb4cea1ee68dd1
Jason Riedy <JJR2834@g.rit.edu>
2022-04-15 16:18:06 -0400

    Theoretically a user can register, but I can't connect to the table.

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /*
 This class deals with the user experience and commands.
  */
@@ -13,6 +15,8 @@ public abstract class App
     private static String email;
     private static String User;
     private static String Password;
+    private static String First_Name;
+    private static String Last_Name;
     private static Scanner scanner;
 
     private static void LOGIN(Connection conn)
@@ -51,17 +55,41 @@ public abstract class App
     {
         System.out.println("Enter your email: ");
         email = scanner.nextLine();
+        System.out.println("Enter your first name: ");
+        First_Name = scanner.nextLine();
+        System.out.println("Enter your last name: ");
+        Last_Name = scanner.nextLine();
+        System.out.println("Enter a username: ");
+        User = scanner.nextLine();
+        System.out.println("Enter a password: ");
+        Password = scanner.nextLine();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        String v = "'" + User + "','" + Password + "','" + First_Name + "','" + Last_Name +
+                "','" + email + "','" + formatter.format(date) + "','" + formatter.format(date) + "'" ;
+        String insertQuery = "insert into \"p320_26.USER\" VALUES ("+ v + ")";
+        try{
+            Statement insertStatement = conn.createStatement();
+            insertStatement.executeUpdate(insertQuery);
+            System.out.println("Registration Success!");
+        }
+        catch(Exception e){
+            System.out.print(e);
+        };
+
+    }
+
+    private static void searchName(Connection conn, String name){
 
     }
 
     public static void UserStart(Connection conn)
     {
-
-        System.out.println("Please enter: 'login'" +
-                " or 'register' or 'quit'" );
         scanner = new Scanner(System.in);
         while(true)
         {
+            System.out.println("Please enter: 'login'" +
+                    " or 'register' or 'quit'" );
             String line = scanner.nextLine();
             if(line.equals(login))
             {
